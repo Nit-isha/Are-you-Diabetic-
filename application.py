840bb20460e50cd41096b52faa14ac3f9bfd289b
Nitisha Gupta <nitishagupta.1999@gmail.com>
2022-06-04 12:22:08 +0530

    preparing for aws deploy

--- application.py
+++ application.py
@@ -2,15 +2,15 @@
 from flask import Flask, render_template, request
 from Main import Check
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
+@application.route("/", methods=['GET'])
 def index_get():
     return render_template('index.html', msg='')
 
 
-@app.route('/', methods=['POST'])
+@application.route('/', methods=['POST'])
 def index_post():
     pregnancy = request.form.get('pregnancy')
     glucose = request.form.get('glucose')
@@ -28,4 +28,4 @@ def index_post():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
